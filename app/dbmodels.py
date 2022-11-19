cfb9d47aec3175ae3236287a1646a978b8a67456
Vadim <V.gorb-123456789@yandex.ru>
2022-11-19 15:30:39 +0300

    Added database models

--- app/dbmodels.py
+++ app/dbmodels.py
@@ -16,7 +16,6 @@ class BaseMeta(ormar.ModelMeta):
     database = database
 
 
-
 class User(ormar.Model):
     class Meta(BaseMeta):
         tablename = "users"
@@ -33,7 +32,6 @@ class UserInfo(ormar.Model):
     info: pydantic.Json = ormar.JSON()
 
 
-
 class UserSettings(ormar.Model):
     class Meta(BaseMeta):
         tablename = "user_settings"
@@ -42,6 +40,72 @@ class UserSettings(ormar.Model):
     settings: pydantic.Json = ormar.JSON()
 
 
+class Message(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "messages"
+    id: int = ormar.Integer(primary_key=True)
+    content: str = ormar.String()
+    created_at: int = ormar.Integer()
+    updated_at: int = ormar.Integer()
+
+
+class Attachment(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "attachments"
+    id: int = ormar.Integer(primary_key=True)
+    message_id: Message = ormar.ForeignKey(Message)
+    files: pydantic.Json = ormar.JSON()
+
+
+class Replice(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "replices"
+    id: int = ormar.Integer(primary_key=True)
+    message_id: Message = ormar.ForeignKey(Message)
+    reply_message_id: Message = ormar.ForeignKey(Message)
+
+
+class Forwarded(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "forwardes"
+    id: int = ormar.Integer(primary_key=True)
+    message_id: Message = ormar.ForeignKey(Message)
+    forwarded_message_id: Message = ormar.ForeignKey(Message)
+
+
+class View(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "views"
+    id: int = ormar.Integer(primary_key=True)
+    message_id: Message = ormar.ForeignKey(Message)
+    user_id: User = ormar.ForeignKey(User)
+
+
+class Reaction(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "reactions"
+    id: int = ormar.Integer(primary_key=True)
+    badge: int = ormar.Integer()
+    description: str = ormar.String()
+
+
+class MessageReaction(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "message_reactions"
+    id: int = ormar.Integer(primary_key=True)
+    message_id: Message = ormar.ForeignKey(Message)
+    user_id: User = ormar.ForeignKey(User)
+    reaction_id: Reaction = ormar.ForeignKey(Reaction)
+
+
+class MessageQueue(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "message_queues"
+    id: int = ormar.Integer(primary_key=True)
+    
+    message_id: Message = ormar.ForeignKey(Message)
+    sender_id: User = ormar.ForeignKey(User)
+    recipient_id: User = ormar.ForeignKey(User)
 
 engine = sqlalchemy.create_engine(config.DATABASE_URL)
 metadata.create_all(engine)

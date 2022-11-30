15433805f6bae230e517d7b67881e5325b553779
Darya Ogolkneva <100416001+woegoru@users.noreply.github.com>
2022-11-30 20:22:10 +0300

    fixed an error in the design of channel paths

--- app/routers/channels.py
+++ app/routers/channels.py
@@ -8,86 +8,86 @@ channels_router = APIRouter(
 )
 
 
-@channels_router.get("/channels")
-async def get_list(info: schemas.Channel):
+@channels_router.get("/")
+async def get_list():
     return {'status':'OK'}
 
 
-@channels_router.post("/channels")
+@channels_router.post("/")
 async def create_channel(info: schemas.Channel):
     return {'status':'OK'}
 
 
-@channels_router.get("/channels/{channel_id}")
+@channels_router.get("/{channel_id}")
 async def get_info_channel(info: schemas.Channel):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.patch("/channels/{channel_id}")
+@channels_router.patch("/{channel_id}")
 async def edit_info_channel(info: schemas.Channel):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.delete("/channels/{channel_id}")
+@channels_router.delete("/{channel_id}")
 async def delete_channel(info: schemas.Channel):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.patch("/channels/{channel_id}/join")
+@channels_router.patch("/{channel_id}/join")
 async def join_channel(info: schemas.Channel):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.patch("/channels/{channel_id}/leave")
+@channels_router.patch("/{channel_id}/leave")
 async def leave_channel():
     return {'channel_id':'channel_id'}
 
 
-@channels_router.get("/channels/{channel_id}/posts")
+@channels_router.get("/{channel_id}/posts")
 async def get_posts(posts: schemas.Post):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.post("/channels/{channel_id}/posts")
+@channels_router.post("/{channel_id}/posts")
 async def create_posts(posts: schemas.Post):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.delete("/channels/{channel_id}/posts")
+@channels_router.delete("/{channel_id}/posts")
 async def delete_posts(posts: schemas.Post):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.patch("/channels/{channel_id}/posts/{post_id}")
+@channels_router.patch("/{channel_id}/posts/{post_id}")
 async def edit_posts(posts: schemas.Post):
     return {'post_id':'post_id'}
 
 
-@channels_router.get("/channels/{channel_id}/members")
+@channels_router.get("/{channel_id}/members")
 async def get_members(posts: schemas.User):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.patch("/channels/{channel_id}/members")
+@channels_router.patch("/{channel_id}/members")
 async def invite_users(posts: schemas.User):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.get("/channels/{channel_id}/my_role")
+@channels_router.get("/{channel_id}/my_role")
 async def get_role(roles: schemas.Channel_Role):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.get("/channels/{channel_id}/roles")
+@channels_router.get("/{channel_id}/roles")
 async def get_list_roles(roles: schemas.Channel_Role):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.post("/channels/{channel_id}/roles")
+@channels_router.post("/{channel_id}/roles")
 async def create_roles(roles: schemas.Channel_Role):
     return {'channel_id':'channel_id'}
 
 
-@channels_router.patch("/channels/{channel_id}/roles")
+@channels_router.patch("/{channel_id}/roles")
 async def change_roles(roles: schemas.Channel_Role):
     return {'channel_id':'channel_id'}

8f8fb115a3265b3e1e55df4af5c354ddeb143258
Darya Ogolkneva <100416001+woegoru@users.noreply.github.com>
2022-11-30 20:42:21 +0300

    added schemes

--- app/schemas.py
+++ app/schemas.py
@@ -25,3 +25,45 @@ class UserlistIn(BaseModel):
     ban: bool
     ignore: bool
     user_ids: Union[None, list[int]]
+        
+        
+class Channel(BaseModel):
+    id: int
+    author_id: int
+    title: str
+    description: str
+    members_count: int
+    photo: Photo
+    create_time: str
+        
+        
+class Post(BaseModel):
+    post_id: str
+    author_id: int
+    channel_id: int
+    text: str
+    attachments: array
+    upload_time: str
+        
+        
+class Channel_Role(BaseModel):
+    id: int
+    title: str
+    name_color: str
+    posts_permissions: int
+    decoration_permissions: int
+    join_permissions: int
+    roling_permissions: array
+        
+        
+class User(BaseModel):
+    id: int
+    username: str
+    firstname: str
+    lastname: str
+    photo: Photo
+    last_time: str
+
+    
+    
+    

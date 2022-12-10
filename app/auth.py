854177d280aa4c74c6eae798908d0d52ef4c5847
Vadim <V.gorb-123456789@yandex.ru>
2022-12-10 22:52:38 +0300

    added class-adapter for JWT authorization

---
+++ app/auth.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
+
+import config
+
+#class-adapter for authorization using JWT
+class JWTAuth():
+    auth_mainclass = AuthJWT
+    auth_exeption = AuthJWTException
+
+    def __init__(self):
+        self.auth = AuthJWT()
+
+    @classmethod
+    def load_settings(cls, settings: BaseModel):
+        cls.auth_mainclass.load_config(settings)
+
+    def get_jwt_subject(self):
+        return self.auth.get_jwt_subject()
+
+    def jwt_required(self):
+        self.auth.jwt_required()
+
+    def create_access_token(self, subject):
+        return self.auth.create_access_token(subject=subject)
+
+    def create_refresh_token(self, subject):
+        return self.auth.create_refresh_token(subject=subject)
+
+
+class Settings(BaseModel):
+        authjwt_secret_key: str = config.JWT_SECRET_KEY
+
+JWTAuth.load_settings(Settings)
